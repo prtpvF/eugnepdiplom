ff052f128554a32f8b03972bf943e15d9024f968
mihail.chaplygin <mihail.chaplygin@intexsoft.by>
2025-06-17 20:40:53 +0300

    update

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import {showError} from 'https://effortless-douhua-d77333.netlify.app/errorMessageModule.js';
-import { API_CONFIG } from "https://effortless-douhua-d77333.netlify.app/constants";
+import { API_CONFIG } from "https://effortless-douhua-d77333.netlify.app/constants.js";
 
 document.addEventListener('DOMContentLoaded', () => {
     const registrationForm = document.getElementById('registrationForm');
