f80893fd516b5ed12f07294800efbcdbbc9a0abb
mihail.chaplygin <mihail.chaplygin@intexsoft.by>
2025-06-18 00:20:04 +0300

    updated

--- constants.js
+++ constants.js
@@ -1,6 +1,6 @@
 export const API_CONFIG = {
-    BASE_URL: 'https://diplom-5sra.onrender.com',
-    FRONT_URL: 'https://effortless-douhua-d77333.netlify.app/api',
+    BASE_URL: 'https://diplom-5sra.onrender.com/api',
+    FRONT_URL: 'https://effortless-douhua-d77333.netlify.app',
     ENDPOINTS: {
         LOGIN: '/auth/login',
         REGISTRATION: '/auth/registration',
